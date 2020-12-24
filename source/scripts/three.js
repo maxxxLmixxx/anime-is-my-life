d3752418512120789e9ccc4c2a09d655cc03c997
maxxxLmixxx.sh <max280599@gmail.com>
2020-12-24 20:49:26 +0300

    init: v-1.0

---
+++ source/scripts/three.js
@@ -0,0 +1,57 @@
+function threeInit({ width, height, root }) {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+        75, width / height, 0.1, 1000
+    )
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    renderer.setSize(width, height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    
+    const loader = new THREE.GLTFLoader();
+
+    document.querySelector(root).appendChild(renderer.domElement)
+    return { scene, camera, renderer, loader }
+}
+
+function render(scene, camera, renderer, callback) {
+    function animate() {
+        requestAnimationFrame(animate);
+        callback(); 
+        renderer.render(scene, camera);
+    }; animate();
+}
+
+const { scene, camera, renderer, loader } = threeInit({
+    width: window.innerWidth, height: window.innerHeight, root: '.root'
+}), controls = new THREE.OrbitControls(camera, renderer.domElement),
+    ambient = new THREE.AmbientLight(0x404040, 1);
+
+loader.load('../assets/3D/scene.gltf', (gltf) => {    
+    scene.add(ambient);
+    scene.add(gltf.scene);
+
+    camera.position.set(-190, 50, 50);
+    
+    gltf.scene.position.x = +25;
+    gltf.scene.position.y = -50;
+    gltf.scene.position.z = -10;
+    
+    const girl3D = gltf.scene.children[0];
+    girl3D.rotation.z = 300;
+
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 5;
+
+    window.addEventListener('resize', onWindowResize);
+    render(scene, camera, renderer, () => {
+        controls.update()
+        // girl3D.rotation.z += 0.01;
+    });
+});
+
+function onWindowResize(e) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectileMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
